e68688b5fef5fafb14857402b642b27c2a46d714
Damon Kaswell <damon.kaswell1@hp.com>
2026-05-26 17:01:21 -0700

    fix(cleanup): remove stray logging line

--- src/handlers/crud.rs
+++ src/handlers/crud.rs
@@ -80,7 +80,6 @@ pub async fn handle_crud(
                     }
                 };
 
-            eprintln!("DEBUG SQL: {} | params: {:?}", built.sql, built.params);
             let rows = match pool.fetch_all_json(&built.sql, &built.params).await {
                 Ok(r) => r,
                 Err(e) => {
